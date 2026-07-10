0368a5c6bc0e544133710d0cea0e1ac5b6d1e7b2
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-10 12:52:01 +0100

    feat(index): create an ASCII title using Figlet before the CLI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
+import figlet from "figlet";
 import { Command } from "commander";
-const program = new Command();
 
+console.log(await figlet.text("ToDo CLI"));
+
+const program: Command = new Command();
 program.name("todo-cli").description("A simple todo list CLI").version("1.0.0");
 
 program.parse();
