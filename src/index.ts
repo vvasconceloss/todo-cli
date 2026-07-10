31afe00d092b860290f651ee72be587d76432b93
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-10 13:29:27 +0100

    feat(index): add a dummy command to the commander’s entry point

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@
 
 import figlet from "figlet";
 import { Command } from "commander";
+import { dummyCommand } from "./commands/dummy.js";
+import TodoService from "./services/todo.service.js";
 
 console.log(await figlet.text("ToDo CLI"));
 
+const todoService = new TodoService();
+
 const program: Command = new Command();
 program.name("todo-cli").description("A simple todo list CLI").version("1.0.0");
+
+dummyCommand(program, todoService);
 
 program.parse();
