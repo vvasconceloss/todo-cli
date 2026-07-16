8ebd990261532ad57d72cae54ebbfeaa7e4ba020
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-16 17:36:37 +0100

    feat(index): remove the dummy command and add the add command

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import figlet from "figlet";
 import { Command } from "commander";
-import { dummyCommand } from "./commands/dummy.js";
+import { addCommand } from "./commands/add.js";
 import TodoService from "./services/todo.service.js";
 
 console.log(await figlet.text("ToDo CLI"));
@@ -12,6 +12,6 @@ const todoService = new TodoService();
 const program: Command = new Command();
 program.name("todo-cli").description("A simple todo list CLI").version("1.0.0");
 
-dummyCommand(program, todoService);
+addCommand(program, todoService);
 
 program.parse();
