df3d20e13bc11b7b9ab762953218f1e98852e208
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-16 17:35:04 +0100

    feat(commands): create a command to add a task with a title

---
+++ src/commands/add.ts
@@ -0,0 +1,12 @@
+import type { Command } from "commander";
+import type TodoService from "../services/todo.service.js";
+
+export function addCommand(program: Command, todoService: TodoService) {
+  program
+    .command("add")
+    .description("add a new task")
+    .requiredOption('--title <title>', 'set the task title')
+    .action((options) => {
+      todoService.add(options.title);
+    });
+}
