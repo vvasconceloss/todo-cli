7cb614f1646c59e4358e0bf07f38d49fa7ceb451
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-21 15:43:51 +0100

    chore(index): use the setup function when starting the CLI

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import figlet from "figlet";
 import { Command } from "commander";
 import { addCommand } from "./commands/add.js";
 import TodoService from "./services/todo.service.js";
+import { setupFile } from "./utils/file.js";
 
 console.log(await figlet.text("ToDo CLI"));
 
 const todoService = new TodoService();
+
+await setupFile();
 
 const program: Command = new Command();
 program.name("todo-cli").description("A simple todo list CLI").version("1.0.0");
