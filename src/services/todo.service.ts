bb26c577d9680080f24e72931f347b354f547a1e
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-07-21 15:49:43 +0100

    feat(services): Implement the addition of tasks to the todo service

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,7 +1,16 @@
 import type { Task } from "../models/todo.js";
+import { getNextId, readFileContent, writeToFile } from "../utils/file.js";
 
 class TodoService {
-  add(title: string): void {}
+  async add(title: string): Promise<void> {
+    let id: number = await getNextId();
+    const data = await readFileContent();
+
+    data.tasks.push({ id, title, completed: false });
+
+    const todoJSON = JSON.stringify(data);
+    await writeToFile(todoJSON);
+  }
 
   list(): Task[] {
     return [];
